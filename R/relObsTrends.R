cd9dbb881a644b4bada11015d7a69f6b96f285cd
Alejandro Ruete <alejandro@greensway.se>
2019-11-01 10:52:21 +0100

    make it uniform to other functions terminology and format

--- R/relObsTrends.R
+++ R/relObsTrends.R
@@ -1,7 +1,7 @@
 #' Relative Observation Trends
 #'
 #' @param x an object of class \sQuote{SummarizeBirds}.
-#' @param scale \code{"Yearly", "Monthly"} or \code{"Daily"}
+#' @param timeRes the time resolution:  \code{"Yearly", "Monthly"} or \code{"Daily"}
 #'
 #' @param focalSp the focal sp to look for.
 #'
@@ -9,26 +9,46 @@
 #' @export
 #'
 #' @examples
-relObsTrends<-function(x, scale, focalSp){
+relObsTrends<-function(x, timeRes, focalSp){
+  if (class(x) != "SummarizedBirds") {
+    stop("The object 'x' must be of class SummarizedBirds.")
+  }
+  if (is.null(focalSp)) {
+    stop("Please, define the focal species to search for.")
+  }
+  timeRes <- tolower(timeRes)
+  yearsAll <- as.numeric(dimnames(x$spatioTemporal)[[2]])
 
   spData<-deconstructOverlay(x$overlaid, attr(x, "visitCol"))
 
-  spData$group<-if(scale=="yearly"){
+  spData$group<-if(timeRes=="yearly"){
     spData$year
-  }else if (scale=="monthly"){
+  }else if (timeRes=="monthly"){
     paste0(spData$year,"-",spData$month)
-  }else if(scale=="daily"){
+  }else if(timeRes=="daily"){
     paste0(spData$year,"-",spData$month,"-",spData$day)
   }else{
-    stop("Unknown scale")
+    stop("Unknown time resolution")
   }
 
-  allN<-dplyr::summarise(dplyr::group_by(spData,group),all=dplyr::n())
-  spN<-dplyr::summarise(dplyr::group_by(spData[spData$scientificName==focalSp,],group),sp=dplyr::n())
+  allN<-summarise(group_by(spData,group),all=n())
+  spN<-summarise(group_by(spData[spData$scientificName==focalSp,],group),sp=n())
   res<-merge(allN, spN, by="group", all.x=TRUE)
 
+  groupYMD<-strsplit(res$group,split = "-")
+  year<-as.numeric(unlist(lapply(groupYMD, function(x) x[1])))
+  month<-as.numeric(unlist(lapply(groupYMD, function(x) x[2])))
+  day<-as.numeric(unlist(lapply(groupYMD, function(x) x[3])))
+
+  rownames(res) <- switch(timeRes,
+                          "yearly" = paste0(year, "-01-01"),
+                          "monthly"= paste0(year, "-", sprintf("%02d", month), "-01"),
+                          "daily"  = paste0(year, "-", sprintf("%02d", month), "-", sprintf("%02d", spData$day)))
+
+  res<-res[,-1]
   res$relObs<-res$sp/res$all
 
+  res <- xts::as.xts(res)
   return(res)
 
 }
