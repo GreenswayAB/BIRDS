a496e11813420bc3cd6044455380cafcd07dff66
Alejandro Ruete <aleruete@users.noreply.github.com>
2021-06-30 00:21:47 +0200

    checking solaris

---
+++ R/rhub checks.R
@@ -0,0 +1,12 @@
+library(rhub)
+validate_email()
+mycheck <- check()
+mycheck$browse()
+mycheck$print()
+mycheck$livelog()
+mycheck$urls()
+
+r-oldrel-windows-ix86+x86_64
+r-patched-solaris-x86
+
+check_for_cran()
